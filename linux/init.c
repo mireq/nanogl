75ed851dd2f379c2034846f0e3bf2d769d789a26
Miroslav Bendík <miroslav.bendik@gmail.com>
2020-05-03 08:45:18 +0200

    Loop without hardcoded cycle count

--- linux/init.c
+++ linux/init.c
@@ -13,15 +13,24 @@
 static void gui(void *data) {
 	ngl_driver_t driver;
 
-	simulator_display_init(&driver, 240, 240, NGL_RGB_888, 240 * 240 * 2);
+	simulator_display_init(&driver, 240, 240, NGL_RGBA, 240 * 240 * 2);
 
-	for (size_t i = 0; i < 100; ++i) {
-		ngl_buffer_t *buf = ngl_get_buffer(&driver);
-		uint8_t *framebuffer = (uint8_t *)buf->buffer;
-		for (size_t i = 0; i < (buf->area.width * buf->area.height) * 3; ++i) {
-			framebuffer[i] = rand() % 0xff;
+	for (size_t frame = 0; frame < 100; ++frame) {
+		while (1) {
+			ngl_buffer_t *buf = ngl_get_buffer(&driver);
+			ngl_color_t *framebuffer = (ngl_color_t *)buf->buffer;
+			for (size_t y = 0; y < buf->area.height; ++y) {
+				for (size_t x = 0; x < buf->area.width; ++x) {
+					framebuffer[x + y * buf->area.width].rgba.r = (x + buf->area.x)*256/driver.width;
+					framebuffer[x + y * buf->area.width].rgba.b = (y + buf->area.y)*256/driver.height;
+					framebuffer[x + y * buf->area.width].rgba.g = (framebuffer[x + y * buf->area.width].rgba.r + framebuffer[x + y * buf->area.width].rgba.b) >> 1;
+				}
+			}
+			ngl_flush(&driver);
+			if (buf->area.y + buf->area.height >= driver.height) {
+				break;
+			}
 		}
-		ngl_flush(&driver);
 	}
 
 	simulator_display_destroy(&driver);
