2d144be937d86ae6f527732428598ee8751991d7
Miroslav Bendík <miroslav.bendik@gmail.com>
2020-05-23 10:45:08 +0200

    Added univeersal frame time measurement code

--- main/gui.c
+++ main/gui.c
@@ -4,66 +4,32 @@
 
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
-#include "freertos/timers.h"
 
 #include "gui.h"
 
 
-static const char *TAG = "gui";
 #ifndef SIMULATOR
-#include "esp32/clk.h"
-static long ccounter_overflow_count = 0;
-static esp_cpu_ccount_t last_ccount = 0;
+#include "esp_timer.h"
 #endif
 
 
-static long long get_ns_time() {
+static long long get_us_time() {
 #ifdef SIMULATOR
 	struct timespec ts;
 	timespec_get(&ts, TIME_UTC);
-	return (long)ts.tv_sec * 1000000000L + ts.tv_nsec;
+	return (long long)ts.tv_sec * 1000000L + ts.tv_nsec / 1000;
 #else
-	esp_cpu_ccount_t ccount = esp_cpu_get_ccount();
-	if (ccount < last_ccount) {
-		ccounter_overflow_count++;
-	}
-	last_ccount = ccount;
-	// Get cycle count normalized to 240 MHz
-	int normalize = 4 - (esp_clk_cpu_freq() / 80000000);
-	long long total_cycle = (((long long)1 << (sizeof(esp_cpu_ccount_t) * 8)) * ccounter_overflow_count + last_ccount) * normalize;
-	return total_cycle * 25 / 2;
+	return esp_timer_get_time();
 #endif
-}
-
-
-static void ns_overflow_check_callback(TimerHandle_t timer) {
-	get_ns_time();
 }
 
 
 void gui_loop(ngl_driver_t *driver) {
-	TimerHandle_t get_ns_timer = xTimerCreate(
-		"ns_overflow_check",
-		1000 / portTICK_PERIOD_MS,
-		pdTRUE,
-		(void *)0,
-		ns_overflow_check_callback
-	);
-	if (get_ns_timer == NULL) {
-		ESP_LOGE(TAG, "Timer ns_overflow_check not created");
-		return;
-	}
-	if (xTimerStart(get_ns_timer, 0) != pdPASS) {
-		ESP_LOGE(TAG, "Timer ns_overflow_check not started");
-		xTimerDelete(get_ns_timer, 0);
-		return;
-	}
-
 	long frame = 0;
 	ngl_buffer_t *buf;
 	while (1) {
 		frame++;
-		//uint64_t ticks_before_frame = esp_cpu_get_ccount();
+		uint64_t us_before_frame = get_us_time();
 		do {
 			buf = ngl_get_buffer(driver);
 			ngl_color_t *framebuffer = (ngl_color_t *)buf->buffer;
@@ -76,11 +42,7 @@ void gui_loop(ngl_driver_t *driver) {
 			}
 			ngl_flush(driver);
 		} while (buf->area.y + buf->area.height < driver->height);
-		//uint64_t ticks_after_frame = esp_cpu_get_ccount();
-		//printf("\nf: %08d, time: %.4f", (int)frame, ((double)ticks_after_frame - (double)ticks_before_frame) / 240000.0);
-		printf("\nf: %08ld, time: %f", frame, (double)(get_ns_time()));
+		uint64_t us_after_frame = get_us_time();
+		printf("\nf: %08ld, time: %.4f ms", frame, (double)(us_after_frame - us_before_frame) / 1000.);
 	}
-
-	xTimerStop(get_ns_timer, 0);
-	xTimerDelete(get_ns_timer, 0);
 }
